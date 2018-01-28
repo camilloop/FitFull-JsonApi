cc83ab02b66c2d2b2421abb42673fc3bcb225dea
camilloop <kamilpaluch@gmail.com>
2018-01-28 11:22:29 +0100

    git init

---
+++ src/main/java/org/camilloop/fitfull/FitfullApplication.java
@@ -0,0 +1,12 @@
+package org.camilloop.fitfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitfullApplication.class, args);
+	}
+}
