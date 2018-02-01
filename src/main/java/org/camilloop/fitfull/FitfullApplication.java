888ea9e4a37d7c5c09e077426b3ec76585b6a60c
camilloop <kamilpaluch@gmail.com>
2018-02-01 21:01:26 +0100

    json api, katharsis configuration

--- src/main/java/org/camilloop/fitfull/FitfullApplication.java
+++ src/main/java/org/camilloop/fitfull/FitfullApplication.java
@@ -1,9 +1,12 @@
 package org.camilloop.fitfull;
 
+import io.katharsis.spring.boot.v3.KatharsisConfigV3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(KatharsisConfigV3.class)
 public class FitfullApplication {
 
 	public static void main(String[] args) {
