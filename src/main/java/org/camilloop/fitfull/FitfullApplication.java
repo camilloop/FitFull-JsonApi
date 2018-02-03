d372be993b9e4b5dd2cdafdb328b13a70fb28184
camilloop <kamilpaluch@gmail.com>
2018-02-03 20:51:22 +0100

    testing data

--- src/main/java/org/camilloop/fitfull/FitfullApplication.java
+++ src/main/java/org/camilloop/fitfull/FitfullApplication.java
@@ -1,15 +1,33 @@
 package org.camilloop.fitfull;
 
 import io.katharsis.spring.boot.v3.KatharsisConfigV3;
+import org.camilloop.fitfull.model.Meal;
+import org.camilloop.fitfull.model.Product;
+import org.camilloop.fitfull.repository.MealRepository;
+import org.camilloop.fitfull.repository.ProductRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
+import java.util.Arrays;
 
 @SpringBootApplication
 @Import(KatharsisConfigV3.class)
 public class FitfullApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FitfullApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(FitfullApplication.class, args);
+
+		//Testing data
+		ProductRepository bean = run.getBean(ProductRepository.class);
+		bean.deleteAll();
+		Product banan = bean.save(new Product("Banan", 312, 12, 32, 23));
+		Product maka = bean.save(new Product("Maka", 210, 11, 3, 1));
+
+		MealRepository bean1 = run.getBean(MealRepository.class);
+		bean1.deleteAll();
+		Meal bananowiec = bean1.save(new Meal("Bananowiec"));
+		bananowiec.setProducts(Arrays.asList(banan, maka));
+		bean1.save(bananowiec);
 	}
 }
